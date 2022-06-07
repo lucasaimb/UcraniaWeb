200e21fc2382069c14693fc746d1f3ab55b66bbf
lucasaimb <103373950+lucasaimb@users.noreply.github.com>
2022-06-07 18:38:14 -0300

    Menu funcionando com js e css

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-window.onload = function(){
+window.onload = function() {
     const menu = document.querySelector('.menu')
     const header = document.querySelector('header')
     const nav = document.querySelector('nav')
 
     menu.onclick = (e) =>{
         header.classList.toggle('on')
-        header.classList.toggle('on')
+        nav.classList.toggle('on')
     }
 }
