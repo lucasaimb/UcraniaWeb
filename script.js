8ff013becd2b18ffbbb6c8acc91aad063ffb4fee
lucasaimb <103373950+lucasaimb@users.noreply.github.com>
2022-06-07 17:34:49 -0300

    Responsivo main finalizado e iniciado js

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    const menu = document.querySelector('.menu')
+    const header = document.querySelector('header')
+    const nav = document.querySelector('nav')
+
+    menu.onclick = (e) =>{
+        header.classList.toggle('on')
+        header.classList.toggle('on')
+    }
+}
